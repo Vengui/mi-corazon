41e1d5e55ad583f840b436ce27ee9097c056e87a
Vengui <marco.salcidojr97@hotmail.com>
2025-02-04 19:51:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 setTimeout(() => {
     document.getElementById('message').style.display = 'block';
+    document.getElementById('buttons').style.display = 'block';
 }, 5000);
+
+document.getElementById('yesButton').addEventListener('click', () => {
+    alert('¡Sabía que dirías que sí! ❤️');
+});
+
+document.getElementById('noButton').addEventListener('click', () => {
+    alert('No es una opción válida 💔');
+});
